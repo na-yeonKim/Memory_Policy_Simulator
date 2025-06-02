d44d9d91e41936d436f67e28c64895f16425293e
na-yeonKim <165547330+na-yeonKim@users.noreply.github.com>
2025-06-02 11:55:40 +0900

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
-﻿using System;
 using System.Windows.Forms;
+using System;
 
 namespace Memory_Policy_Simulator
 {
@@ -23,15 +23,6 @@ namespace Memory_Policy_Simulator
             base.Dispose(disposing);
         }
 
-        private void tbWindowSize_KeyDown(object sender, KeyEventArgs e)
-        {
-        }
-
-        private void chart1_Click(object sender, EventArgs e)
-        {
-        }
-
-
         #region Windows Form 디자이너에서 생성한 코드
 
         /// <summary>
@@ -41,9 +32,9 @@ namespace Memory_Policy_Simulator
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btnOperate = new System.Windows.Forms.Button();
             this.tbWindowSize = new System.Windows.Forms.TextBox();
             this.tbQueryString = new System.Windows.Forms.TextBox();
@@ -65,9 +56,10 @@ namespace Memory_Policy_Simulator
             // btnOperate
             // 
             this.btnOperate.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.btnOperate.Location = new System.Drawing.Point(1031, 7);
+            this.btnOperate.Location = new System.Drawing.Point(555, 4);
+            this.btnOperate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOperate.Name = "btnOperate";
-            this.btnOperate.Size = new System.Drawing.Size(142, 70);
+            this.btnOperate.Size = new System.Drawing.Size(76, 35);
             this.btnOperate.TabIndex = 0;
             this.btnOperate.Text = "Run";
             this.btnOperate.UseVisualStyleBackColor = true;
@@ -77,9 +69,10 @@ namespace Memory_Policy_Simulator
             // 
             this.tbWindowSize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbWindowSize.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.tbWindowSize.Location = new System.Drawing.Point(766, 33);
+            this.tbWindowSize.Location = new System.Drawing.Point(412, 16);
+            this.tbWindowSize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbWindowSize.Name = "tbWindowSize";
-            this.tbWindowSize.Size = new System.Drawing.Size(117, 34);
+            this.tbWindowSize.Size = new System.Drawing.Size(64, 21);
             this.tbWindowSize.TabIndex = 1;
             this.tbWindowSize.Text = "4";
             this.tbWindowSize.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbWindowSize_KeyDown);
@@ -89,48 +82,56 @@ namespace Memory_Policy_Simulator
             // 
             this.tbQueryString.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbQueryString.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.tbQueryString.Location = new System.Drawing.Point(174, 33);
+            this.tbQueryString.Location = new System.Drawing.Point(94, 16);
+            this.tbQueryString.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbQueryString.Name = "tbQueryString";
-            this.tbQueryString.Size = new System.Drawing.Size(586, 34);
+            this.tbQueryString.Size = new System.Drawing.Size(316, 21);
             this.tbQueryString.TabIndex = 2;
             // 
             // pImage
             // 
             this.pImage.AutoScroll = true;
             this.pImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pImage.Location = new System.Drawing.Point(12, 97);
+            this.pImage.Location = new System.Drawing.Point(6, 48);
+            this.pImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pImage.Name = "pImage";
-            this.pImage.Size = new System.Drawing.Size(871, 629);
+            this.pImage.Size = new System.Drawing.Size(470, 316);
             this.pImage.TabIndex = 4;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
             // 
             // tbConsole
             // 
             this.tbConsole.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.tbConsole.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.tbConsole.Location = new System.Drawing.Point(889, 97);
+            this.tbConsole.Location = new System.Drawing.Point(479, 48);
+            this.tbConsole.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbConsole.Multiline = true;
             this.tbConsole.Name = "tbConsole";
             this.tbConsole.ReadOnly = true;
-            this.tbConsole.Size = new System.Drawing.Size(393, 203);
+            this.tbConsole.Size = new System.Drawing.Size(213, 102);
             this.tbConsole.TabIndex = 5;
             // 
             // chart1
             // 
-            chartArea2.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
-            chartArea2.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(889, 306);
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(479, 153);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Light;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(393, 390);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(212, 195);
             this.chart1.TabIndex = 6;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
@@ -138,9 +139,10 @@ namespace Memory_Policy_Simulator
             // btnRand
             // 
             this.btnRand.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.btnRand.Location = new System.Drawing.Point(889, 6);
+            this.btnRand.Location = new System.Drawing.Point(479, 3);
+            this.btnRand.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRand.Name = "btnRand";
-            this.btnRand.Size = new System.Drawing.Size(136, 71);
+            this.btnRand.Size = new System.Drawing.Size(73, 36);
             this.btnRand.TabIndex = 7;
             this.btnRand.Text = "Random";
             this.btnRand.UseVisualStyleBackColor = true;
@@ -149,9 +151,10 @@ namespace Memory_Policy_Simulator
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.btnSave.Location = new System.Drawing.Point(1179, 6);
+            this.btnSave.Location = new System.Drawing.Point(635, 3);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(108, 71);
+            this.btnSave.Size = new System.Drawing.Size(58, 36);
             this.btnSave.TabIndex = 7;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -162,9 +165,10 @@ namespace Memory_Policy_Simulator
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "FIFO"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 34);
+            this.comboBox1.Location = new System.Drawing.Point(6, 17);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 32);
+            this.comboBox1.Size = new System.Drawing.Size(67, 20);
             this.comboBox1.TabIndex = 8;
             this.comboBox1.Text = "FIFO";
             // 
@@ -172,9 +176,10 @@ namespace Memory_Policy_Simulator
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.label1.Location = new System.Drawing.Point(39, 2);
+            this.label1.Location = new System.Drawing.Point(21, 1);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 27);
+            this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "Policy";
             // 
@@ -182,9 +187,10 @@ namespace Memory_Policy_Simulator
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.label2.Location = new System.Drawing.Point(377, 2);
+            this.label2.Location = new System.Drawing.Point(203, 1);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(174, 27);
+            this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 9;
             this.label2.Text = "Reference String";
             // 
@@ -192,9 +198,10 @@ namespace Memory_Policy_Simulator
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.label3.Location = new System.Drawing.Point(771, 1);
+            this.label3.Location = new System.Drawing.Point(415, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 27);
+            this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "#Frame";
             // 
@@ -202,9 +209,10 @@ namespace Memory_Policy_Simulator
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.label4.Location = new System.Drawing.Point(889, 699);
+            this.label4.Location = new System.Drawing.Point(479, 350);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(240, 27);
+            this.label4.Size = new System.Drawing.Size(120, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Page Fault Rate (%) = ";
             // 
@@ -212,16 +220,17 @@ namespace Memory_Policy_Simulator
             // 
             this.lbPageFaultRatio.AutoSize = true;
             this.lbPageFaultRatio.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.lbPageFaultRatio.Location = new System.Drawing.Point(1121, 699);
+            this.lbPageFaultRatio.Location = new System.Drawing.Point(604, 350);
+            this.lbPageFaultRatio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbPageFaultRatio.Name = "lbPageFaultRatio";
-            this.lbPageFaultRatio.Size = new System.Drawing.Size(0, 27);
+            this.lbPageFaultRatio.Size = new System.Drawing.Size(0, 13);
             this.lbPageFaultRatio.TabIndex = 9;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1299, 739);
+            this.ClientSize = new System.Drawing.Size(699, 370);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lbPageFaultRatio);
             this.Controls.Add(this.label4);
@@ -237,6 +246,7 @@ namespace Memory_Policy_Simulator
             this.Controls.Add(this.tbWindowSize);
             this.Controls.Add(this.btnOperate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
