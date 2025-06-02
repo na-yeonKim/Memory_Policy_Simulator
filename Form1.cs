2b0e3b09076bed53c9843c9475be9c97700942c1
na-yeonKim <165547330+na-yeonKim@users.noreply.github.com>
2025-06-02 11:43:10 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -36,10 +36,12 @@ namespace Memory_Policy_Simulator
 
         private void DrawBase(dynamic core, int windowSize, int dataLength)
         {
+            /* parse window */
             var psudoQueue = new Queue<char>();
+            
             g.Clear(Color.Black);
 
-            for (int i = 0; i < dataLength; i++)
+            for (int i = 0; i < dataLength; i++) // length
             {
                 int psudoCursor = core.pageHistory[i].loc;
                 char data = core.pageHistory[i].data;
@@ -56,17 +58,22 @@ namespace Memory_Policy_Simulator
                         break;
                 }
 
-                for (int j = 0; j <= windowSize; j++)
+                for ( int j = 0; j <= windowSize; j++) // height - STEP
                 {
                     if (j == 0)
+                    {
                         DrawGridText(i, j, data);
+                    }
                     else
+                    {
                         DrawGrid(i, j);
+                    }
                 }
-
+                
                 DrawGridHighlight(i, psudoCursor, status);
                 int depth = 1;
-                foreach (char t in psudoQueue)
+                
+                foreach ( char t in psudoQueue )
                 {
                     DrawGridText(i, depth++, t);
                 }
@@ -75,23 +82,32 @@ namespace Memory_Policy_Simulator
 
         private void DrawGrid(int x, int y)
         {
-            int gridSize = 30, gridSpace = 5;
-            int gridBaseX = x * gridSize, gridBaseY = y * gridSize;
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+        
             g.DrawRectangle(new Pen(Color.White), new Rectangle(
                 gridBaseX + (x * gridSpace),
                 gridBaseY,
                 gridSize,
-                gridSize));
+                gridSize
+                ));
         }
 
         private void DrawGridHighlight(int x, int y, Page.STATUS status)
         {
-            int gridSize = 30, gridSpace = 5;
-            int gridBaseX = x * gridSize, gridBaseY = y * gridSize;
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+        
             SolidBrush highlighter = new SolidBrush(Color.LimeGreen);
-
+        
             switch (status)
             {
+                case Page.STATUS.HIT:
+                    break;
                 case Page.STATUS.MIGRATION:
                     highlighter.Color = Color.Purple;
                     break;
@@ -99,28 +115,35 @@ namespace Memory_Policy_Simulator
                     highlighter.Color = Color.Red;
                     break;
             }
-
+        
             g.FillRectangle(highlighter, new Rectangle(
                 gridBaseX + (x * gridSpace),
                 gridBaseY,
                 gridSize,
-                gridSize));
+                gridSize
+                ));
         }
 
         private void DrawGridText(int x, int y, char value)
         {
-            int gridSize = 30, gridSpace = 5;
-            int gridBaseX = x * gridSize, gridBaseY = y * gridSize;
-            g.DrawString(value.ToString(),
-                new Font(FontFamily.GenericMonospace, 8),
-                new SolidBrush(Color.White),
-                new PointF(gridBaseX + (x * gridSpace) + gridSize / 3,
-                           gridBaseY + gridSize / 4));
+            int gridSize = 30;
+            int gridSpace = 5;
+            int gridBaseX = x * gridSize;
+            int gridBaseY = y * gridSize;
+        
+            g.DrawString(
+                value.ToString(), 
+                new Font(FontFamily.GenericMonospace, 8), 
+                new SolidBrush(Color.White), 
+                new PointF(
+                    gridBaseX + (x * gridSpace) + gridSize / 3,
+                    gridBaseY + gridSize / 4));
         }
 
         private void btnOperate_Click(object sender, EventArgs e)
         {
             this.tbConsole.Clear();
+            
             if (this.tbQueryString.Text != "" && this.tbWindowSize.Text != "")
             {
                 string data = this.tbQueryString.Text;
@@ -193,6 +216,7 @@ namespace Memory_Policy_Simulator
                 this.pbPlaceHolder.Refresh();
 
                 int total = policy.hit + policy.fault;
+                /* 차트 생성 */
                 chart1.Series.Clear();
                 Series resultChartContent = chart1.Series.Add("Statics");
                 resultChartContent.ChartType = SeriesChartType.Pie;
@@ -206,27 +230,57 @@ namespace Memory_Policy_Simulator
 
                 this.lbPageFaultRatio.Text = Math.Round(((float)policy.fault / total), 2) * 100 + "%";
             }
+            else
+            {
+            }
         }
 
+        private void pbPlaceHolder_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        
+        private void chart1_Click(object sender, EventArgs e)
+        {
+        
+        }
+        
+        private void tbWindowSize_KeyDown(object sender, KeyEventArgs e)
+        {
+        
+        }
+        
         private void tbWindowSize_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
-                e.Handled = true;
+                if (!(Char.IsDigit(e.KeyChar)) && e.KeyChar != 8)
+                {
+                    e.Handled = true;
+                }
         }
-
+        
         private void btnRand_Click(object sender, EventArgs e)
         {
             Random rd = new Random();
+        
             int count = rd.Next(5, 50);
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < count; i++)
+        
+        
+            for ( int i = 0; i < count; i++ )
+            {
                 sb.Append((char)rd.Next(65, 90));
+            }
+        
             this.tbQueryString.Text = sb.ToString();
         }
-
+        
         private void btnSave_Click(object sender, EventArgs e)
         {
             bResultImage.Save("./result.jpg");
+        }
+        
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+        
         }
     }
 }
